b7de9242a3c427368270a1fc52a8fa499b9a907c
BharadwajRachakonda <rbharadwaj022@gmail.com>
2024-10-17 19:59:03 +0530

    Adding dark and light mode

--- App.tsx
+++ App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
 
-function App() {
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'light';
   return (
-    <SafeAreaView>
-      <Text>Hello World</Text>
-    </SafeAreaView>
+    <View
+      style={[
+        styles.container,
+        {backgroundColor: isDarkMode ? 'black' : 'white'},
+      ]}>
+      <Text style={isDarkMode ? styles.dark : styles.white}>Hello World</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  white: {
+    color: 'black',
+  },
+  dark: {
+    color: 'white',
+  },
+});
 
 export default App;
---
+++ components/Test1.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+function Test1() {
+  return <Text>test1</Text>;
+}
+
+export default Test1;
